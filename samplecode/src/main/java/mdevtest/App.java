8af41f53b15a3f43e2e30fec91bd265123c2b720
Aravindkumar P <aravind_p@blr.trigent.com>
2022-02-15 13:28:48 +0530

    commiting

---
+++ samplecode/src/main/java/mdevtest/App.java
@@ -0,0 +1,13 @@
+package mdevtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
