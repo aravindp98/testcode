3a47a07ac3f4d59a16acda61d457a79d480ac0f3
RadhaCSwamy <radha_c@trigent.com>
2023-09-15 10:10:35 +0530

    trgger job

--- trigger_job.py
+++ trigger_job.py
@@ -1,12 +1,12 @@
 import jenkins
 import os
-
+import jenkinsapi
 
 jenkins_url = jenkins.Jenkins("http://172.31.10.31:8080/", username=os.environ.get('new_jenkins_usr'), password=os.environ.get('new_jenkins_pass'))
 
 
 def trigget_build():
-    parameter = {'repository': 'https://gitlab.com/aravindp98/build_trigger.git'}
+    parameter = {'repository': 'https://gitlab.com/dev-ops24/Demo-test.git'}
     jenkins_jobs = jenkins_url.build_job(name="sonarqube_gitlab", parameters=parameter)
 
 trigget_build()
